1992174d47bb3541e5b03964fc97bd2436852e5b
Vladislav Karas <dncmorgan@yandex.ru>
2022-10-11 22:29:44 +0300

    Updates

---
+++ precache-manifest.8d1c16f74d2ab8b33c69a4eab2cc9843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1f0a2ec21177d18bb6f69025fd1778",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "97cfaed7379236132480",
+    "url": "/ToDoList/static/css/main.5c4fb441.chunk.css"
+  },
+  {
+    "revision": "bbe23e4e604ea28b4a1b",
+    "url": "/ToDoList/static/js/2.5e0d8f52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.5e0d8f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cfaed7379236132480",
+    "url": "/ToDoList/static/js/main.261850b9.chunk.js"
+  },
+  {
+    "revision": "86c4286c12cc5c044c93",
+    "url": "/ToDoList/static/js/runtime-main.1aff77a7.js"
+  }
+]);
